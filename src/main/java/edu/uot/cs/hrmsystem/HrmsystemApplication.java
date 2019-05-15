2ccc636c845d09352e85562e900ce39dc0a234d7
Mukhtar Hussain Baloch <mukhtar@uot.edu.pk>
2019-05-15 20:55:20 +0500

    initial commit

---
+++ src/main/java/edu/uot/cs/hrmsystem/HrmsystemApplication.java
@@ -0,0 +1,13 @@
+package edu.uot.cs.hrmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/uot/cs/hrmsystem/HrmsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.uot.cs.hrmsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HrmsystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
